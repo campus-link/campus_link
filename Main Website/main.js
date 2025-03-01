da77d5357642aa363210bf4cca6f2393d73dcff7
kingnstar <145896462+kingnstar@users.noreply.github.com>
2025-03-02 00:00:16 +0530

    uneccesary files deleted like main website clone till first iteration of homepage where only navbar was there

--- Main Website/main.js
+++
@@ -1,9 +0,0 @@
-
-
-function openModal(role) {
-    document.getElementById("modal-title").innerText = "Login as " + role;
-    document.getElementById("loginModal").style.display = "flex";
-}
-function closeModal() {
-    document.getElementById("loginModal").style.display = "none";
-}
